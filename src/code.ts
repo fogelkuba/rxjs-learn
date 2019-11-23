d3a182c3fdf00e0600271bff1a78efaccc162e38
fogelkuba <fogelkuba@gmail.com>
2019-11-23 20:31:13 +0100

    code

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,16 @@
-import * as Rx from "rxjs/Observable";
+// import * as Rx from "rxjs/Observable";
+// console.log(Rx);
 
-console.log(Rx);
+import { Observable } from "rxjs";
+
+// const subscribe = function (observer: any) {
+//     observer.next('Hey guys')
+// };
+
+const observable = Observable.create((observer: any) => {
+    observer.next('Hey guys')
+});
+
+observable.subscribe((v:any) => {
+    console.log(v)
+});
